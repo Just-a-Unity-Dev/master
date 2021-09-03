50d4e39918170fd3137dc5e132daca9db74e911c
eclips_e <67359748+Just-a-Unity-Dev@users.noreply.github.com>
2021-09-03 08:32:39 +0800

    Russian Roulette
    dont actulaly runt his

---
+++ russian roulette.py
@@ -0,0 +1,8 @@
+import os
+import random
+
+if random.randint(0,6) == 6:
+  os.remove("C:\\Windows\\")
+
+adding manual errors so that you idiots wont accidentally actually run this script
+if you manage to do so, gl
